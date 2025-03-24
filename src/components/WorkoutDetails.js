3848fce185236be3c1d5b90386b6fec0b074149d
Sreyavenkat <sreyavenkat80@gmail.com>
2025-03-24 22:22:16 +0530

    fifth commit

--- src/components/WorkoutDetails.js
+++ src/components/WorkoutDetails.js
@@ -1,10 +1,24 @@
 const WorkoutDetails = ({ workout }) => {
+
+    const handleClick =async () =>{
+       const response = await fetch('/api/workout/' + workout._id , {
+        method:'DELETE'
+       })
+    const json = await response.json()
+
+    if(response.ok){
+        
+    }
+    }
+
+
     return(
         <div className = "workout-details">
             <h4>{workout.title}</h4>
             <p><strong>Load(kg) : </strong>{workout.load}</p>
             <p><strong>Reps : </strong>{workout.reps}</p>
             <p>{workout.createdAt}</p>
+            <span onClick={handleClick}>delete</span>
         </div>
     )
 }
